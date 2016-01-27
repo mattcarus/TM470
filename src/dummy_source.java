37b741a2bf5b8f2d4c7fa22992f61e2e8158f4b6
Matt Carus <mattcarus@gmail.com>
2016-01-27 20:18:59 +0000

    Initial commit

---
+++ src/dummy_source.java
@@ -0,0 +1,11 @@
+/*
+ *  Dummy source code listing
+ */
+class Dummy
+{
+	private String name = "Dummy";
+	public Dummy()
+	{
+		
+	}
+}
---
+++ src/report.lol
@@ -0,0 +1,1 @@
+\contentsline {lstlisting}{\numberline {5.1}dummy_source.java}{5}{lstlisting.5.1}
