4bd92198f80edf277e9b4a72bd80e75930dc9a6f
Matt Carus <mattcarus@gmail.com>
2016-01-27 21:00:26 +0000

    Fixed source code highlighting issue

--- src/dummy_source.java
+++ src/dummy_source.java
@@ -1,10 +1,21 @@
-/*
- *  Dummy source code listing
+/** 
+ * Testing block comments
+ * 
+ * On multiple lines
+ * 
  */
+
+
 class Dummy
 {
 	private String name = "Dummy";
 	public Dummy()
+	{
+		
+	}
+	
+	@Override
+	public void test()
 	{
 		
 	}
