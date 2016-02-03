474eedf26f15c0058cd689e1ab9d655c7b378177
Matt Carus <mattcarus@gmail.com>
2016-02-03 21:35:07 +0000

    Added Gantt Chart and update build script to include SVG to pdf_tex conversion.

--- src/build.sh
+++ src/build.sh
@@ -8,3 +8,12 @@ latex2rtf -o ../build/report.rtf -T ../tmp -b ../tmp/report.bbl report.tex
 mv ../tmp/report.pdf ../build/
 rm -f ../tmp/*
 cd ..
+
+# To build SVG images (e.g. Gantt Chart)
+#
+#  inkscape -D -z --file=gantt.svg --export-pdf=gantt.pdf --export-latex
+#
+# Then, in tex file, use 
+#
+#  \def\svgwidth{350px}		% Only if image needs to be scaled
+#  \input{gantt.pdf_tex}
